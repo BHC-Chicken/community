13ef846087b654d2290a0cee7933462d3ba30b46
BHC-Chicken <tij05078@gmail.com>
2022-04-15 22:15:03 +0900

    RegExp

--- src/main/resources/static/script/signupError.js
+++ src/main/resources/static/script/signupError.js
@@ -3,9 +3,10 @@ const PASSWORD = "^([0-9a-zA-Z`~!@#$%^&*()\\-_=+\\[{\\]}\\\\|;:'\",<.>/?]{8,50})
 const NICKNAME = "^([0-9a-zA-Z가-힣]{1,10})$";
 
 const formSignUp = window.document.body.querySelector('[rel="form-signup"]');
-formSignUp.submit = () => {
+formSignUp.onsubmit = () => {
     const username = document.getElementById('username').value;
     if (!username.match(USERNAME)){
+        console.log(username)
         alert('올바른 아이디를 입력해주세요.')
     }
 
