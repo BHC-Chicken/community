84a0e55021575daa7865475c40fe1a52059d9139
BHC-Chicken <tij05078@gmail.com>
2022-10-31 01:04:21 +0900

    redis에 섹션 저장 하도록 변경

--- src/main/java/com/peachdevops/community/CommunityApplication.java
+++ src/main/java/com/peachdevops/community/CommunityApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableAsync
+@EnableRedisHttpSession
 public class CommunityApplication extends SpringBootServletInitializer {
 
     @Override protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
